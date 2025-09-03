8392a6d8764315ff7b5de52fc95dfb00abee5d65
Eric Huang <erhuang623@gmail.com>
2025-09-02 18:31:46 -0700

    Initial commit

---
+++ src/components/GameBoard.tsx
@@ -0,0 +1,19 @@
+type GameBoardProps = {
+  board: number[][];
+};
+
+export default function GameBoard({ board }: GameBoardProps) {
+  return (
+    <div className="inline-grid bg-gray-800 p-2 rounded-lg"
+         style={{ gridTemplateColumns: `repeat(${board[0].length}, 1fr)` }}>
+      {board.flat().map((cell, idx) => (
+        <div
+          key={idx}
+          className={`w-6 h-6 border border-gray-700 ${
+            cell ? "bg-blue-500" : "bg-gray-900"
+          }`}
+        />
+      ))}
+    </div>
+  );
+}
